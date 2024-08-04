0be834e452cab102b95d6e19b9c93a6fd6a4c34a
UzurpatorUzbekistanu <121967726+UzurpatorUzbekistanu@users.noreply.github.com>
2024-08-04 18:52:32 +0200

    Lesson2 (#2)
    * CI
    
    * Struktura projektu bez testów
    
    * dodano lombok.config
    
    * Realizacja zadania z lekcji 2

---
+++ app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -0,0 +1,48 @@
+package pl.futurecollars.invoicing.db.memory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import pl.futurecollars.invoicing.db.Database;
+import pl.futurecollars.invoicing.model.Invoice;
+
+public class InMemoryDatabase implements Database {
+
+  private final Map<Integer, Invoice> invoices = new HashMap<>();
+  private int nextId = 1;
+
+  @Override
+  public int save(Invoice invoice) {
+    invoice.setId(nextId);
+    invoices.put(nextId, invoice);
+    return nextId++;
+  }
+
+  @Override
+  public Optional<Invoice> getById(int id) {
+    return Optional.ofNullable(invoices.get(id));
+  }
+
+  @Override
+  public List<Invoice> getAll() {
+    return invoices.values().stream().collect(Collectors.toList());
+  }
+
+  @Override
+  public void update(int id, Invoice updatedInvoice) {
+    if (!invoices.containsKey(id)) {
+      throw new IllegalArgumentException("Id " + id + " does not exist");
+    }
+
+    invoices.replace(id, updatedInvoice);
+  }
+
+  @Override
+  public void delete(int id) {
+    if (invoices.containsKey(id)) {
+      invoices.remove(id);
+    }
+  }
+}
