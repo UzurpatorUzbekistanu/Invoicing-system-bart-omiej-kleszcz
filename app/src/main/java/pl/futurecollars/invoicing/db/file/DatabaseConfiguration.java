7d317d7bf970ea7c793a60f321a7d87cb6c423a1
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-09-10 16:17:11 +0200

    Realizacja lekcji numer 7

--- app/src/main/java/pl/futurecollars/invoicing/db/file/DatabaseConfiguration.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/file/DatabaseConfiguration.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,8 @@ import pl.futurecollars.invoicing.utils.JsonService;
 @Slf4j
 @Configuration
 public class DatabaseConfiguration {
+
+  private final Logger logger = LoggerFactory.getLogger(DatabaseConfiguration.class);
 
   @Bean
   public IdProvider idProvider(FilesService filesService,
