ed87ea544cb44b8190165fb68524f60e99061f69
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-09-11 19:18:45 +0200

    poprawki checkstyle w SpringFoxConfig

--- app/src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfig.java
+++ app/src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfig.java
@@ -9,25 +9,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringFoxConfig {
-
   @Bean
   public OpenApiCustomizer openApiCustomizer() {
     return openApi -> {
-      openApi
-          .info(new Info()
-              .description("Application to manage set of invoices")
-              .license(new io.swagger.v3.oas.models.info.License().name("No license available - private!"))
-              .title("Private invoicing")
-              .contact(new Contact()
-                  .name("Bartłomiej Kleszcz")
-                  .url("https://github.com/UzurpatorUzbekistanu")
-                  .email("bartłomiej@wp.pl"))
-          );
+      openApi.info(new Info().description("Application to manage set of invoices")
+          .license(new io.swagger.v3.oas.models.info.License().name("No license available - private!")).title("Private invoicing")
+          .contact(new Contact().name("Bartłomiej Kleszcz").url("https://github.com/UzurpatorUzbekistanu").email("bartłomiej@wp.pl")));
 
-      openApi.addTagsItem(new Tag()
-          .name("invoice-controller")
-          .description("Controller used to list / add / update / delete invoices (CRUD)")
-      );
+      openApi.addTagsItem(new Tag().name("invoice-controller").description("Controller used to list / add / update / delete invoices (CRUD)"));
     };
   }
 }
