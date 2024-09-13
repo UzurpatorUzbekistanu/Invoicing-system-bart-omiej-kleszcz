f2177683e7a0a3625cf6ea0cc8a084c32e36314f
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-09-13 08:28:12 +0200

    realizacja lekcji 9

---
+++ src/main/java/pl/futurecollars/invoicing/service/TaxCalculatorResult.java
@@ -0,0 +1,23 @@
+package pl.futurecollars.invoicing.service;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@Builder
+@Data
+public class TaxCalculatorResult {
+
+  private final BigDecimal income;
+  private final BigDecimal costs;
+  private final BigDecimal earnings;
+
+  private final BigDecimal incomingVat;
+  private final BigDecimal outgoingVat;
+  private final BigDecimal vatToReturn;
+
+}
