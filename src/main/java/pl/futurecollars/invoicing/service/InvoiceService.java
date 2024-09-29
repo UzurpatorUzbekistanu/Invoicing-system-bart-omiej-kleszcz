50372d9e949013dc4cb76194958b1f0e6ee02a37
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-09-29 11:36:01 +0200

    realizacja lekcji 12

--- src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
+
 @Service
 public class InvoiceService {
 
