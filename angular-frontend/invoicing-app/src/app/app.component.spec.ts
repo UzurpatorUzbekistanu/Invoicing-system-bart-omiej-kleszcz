b4edd9e7eb3b0bd7576a4c3868dec8cb53a0c666
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-10-25 11:56:32 +0200

    realizacja lekcji 18

--- angular-frontend/invoicing-app/src/app/app.component.spec.ts
+++ angular-frontend/invoicing-app/src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CompanyService } from './company.service';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+ beforeEach(async () => {
+     await TestBed.configureTestingModule({
+         declarations: [AppComponent],
+         imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+         providers: [CompanyService],
+         schemas: [NO_ERRORS_SCHEMA]
+     }).compileComponents();
+ });
+
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -25,5 +34,12 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, invoicing-app');
+  });
+
+  it('should render the companies table', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('#companiesTable')).toBeTruthy();
   });
 });
